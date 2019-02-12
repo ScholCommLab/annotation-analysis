9eef21a0cdb8b0ea918d2c05f77b1c4966dadf82
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2019-02-11 16:49:38 -0800

    improved collection pipeline

--- code/collect_comments.py
+++ code/collect_comments.py
@@ -4,90 +4,83 @@ from pathlib import Path
 import pandas as pd
 import requests
 import urltools
-from hypothesisapi import *
 from tqdm import tqdm
+import json
 
 
-def query_altmetric(url, start):
-    """
-    Collect hypothesis data for those readings
-    """
-    params = {'url': url, 'limit': 200, 'offset': start}
+class HypothesisAPI(object):
+    def __init__(self, api_key):
+        self.api_url = "https://hypothes.is/api"
+        self.search_url = self.api_url + "/search"
+        self.api_key = api_key
 
-    r = requests.get(API_URL + "/search", headers=headers, params=params)
-    return r.json()
+        self.headers = {
+            'Authorization': 'Bearer ' + self.api_key,
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+
+    def search(self, params):
+        """
+        Collect hypothesis data for those readings
+        """
+        r = requests.get(
+            self.search_url,
+            headers=self.headers,
+            params=params)
+
+        return r.json()
 
 
 if __name__ == "__main__":
-    # Load config
+    # Init
     Config = configparser.ConfigParser()
     Config.read('../config.cnf')
 
     api_key = Config.get('hypothesis', 'api_key')
-    API_URL = "https://hypothes.is/api"
-    headers = {'Authorization': 'Bearer ' + api_key,
-               'Content-Type': 'application/json;charset=utf-8'}
+    api = HypothesisAPI(api_key)
 
+    # Load input data
     data_dir = Path("../data")
-    input_files = list(data_dir.glob("Hypothesis*.csv"))
+    readings = pd.read_csv(data_dir / "readings.csv")
 
-    # Initialise output dataframe for individual readings
-    readings_cols = ["course", "instructor", "date",
-                     "month", "week", "title", "url", "total", "resp"]
-    readings = pd.DataFrame(columns=readings_cols)
+    # Query each URL/group for comments
+    response_cols = ["total", "resp"]
+    responses = pd.DataFrame(columns=response_cols)
 
-    dfs = []
-    for f in input_files:
-        try:
-            df = pd.read_csv(f, parse_dates=["Date"])
-            df['month'] = df.Date.map(lambda x: x.month)
-            df['week'] = df.Date.map(lambda x: x.week)
-            df['week'] = df['week'] - min(df['week']) + 1
-        except Exception as e:
-            print("Skipping {}".format(f.name))
-            continue
-
-        dfs.append(df)
-
-        course = df["Course Number"][0]
-        instructor = df["First Name"][0] + " " + df["Last Name "][0]
-
-        for ix, row in df.iterrows():
-            readings.loc[len(readings)] = [course, instructor, row['Date'], row['month'],
-                                           row['week'], row['Title'], row['Link '], None, None]
-
-    df = pd.concat(dfs, sort=False)
-
-    classes_cols = ["Course Number", "Course Name", "First Name", "Last Name ", "Email"]
-    classes = df.groupby(classes_cols)["Link "].count().reset_index()
-    classes.to_csv("../data/courses.csv")
-
-    responses = []
     for ix, row in tqdm(list(readings.iterrows()), desc="Querying Hypothes.is"):
         rows = []
         offset = 0
-        resp = query_altmetric(row['url'], offset)
+        group = row['group']
+
+        params = {
+            'url': row['url'],
+            'limit': 200,
+            'offset': offset,
+            'group': group
+        }
+
+        resp = api.search(params)
         total = resp['total']
         rows.extend(resp['rows'])
         while len(rows) < total:
-            offset = offset + 200
-            resp = query_altmetric(row['url'], offset)
+            params['offset'] = params['offset'] + 200
+            resp = api.search(params)
             rows.extend(resp['rows'])
-        readings.loc[ix, 'resp'] = json.dumps(rows)
-        readings.loc[ix, 'total'] = total
 
-    readings.index.name = "id"
-    readings.to_csv("../data/readings.csv")
+        responses.loc[ix] = [total, json.dumps(rows)]
+    responses.index.name = "reading_id"
+    responses.to_csv(data_dir / "hypothesis_responses.csv")
 
     # Create dataframe for each comment
-    comments_cols = ["url_id", "user", "text", "created", "updated", "references"]
+    comments_cols = ["hypothesis_id", "reading_id",
+                     "user", "text", "created", "updated", "references"]
     comments = pd.DataFrame(columns=comments_cols)
 
-    for ix, row in tqdm(list(readings.iterrows()), desc="Parsing annotations"):
+    for ix, row in tqdm(list(responses.iterrows()), desc="Parsing annotations"):
         rows = json.loads(row['resp'])
-        url_id = int(ix)
+        reading_id = int(ix)
         for r in rows:
-            id = r['id']
+            hypothesis_id = r['id']
             user = r['user'][5:-12]
             text = "".join(r['text'])
             created = pd.datetime.strptime(r['created'].split("+")[0], "%Y-%m-%dT%H:%M:%S.%f")
@@ -97,7 +90,8 @@ if __name__ == "__main__":
             else:
                 references = None
 
-            comments.loc[id] = [url_id, user, text, created, updated, references]
+            comment = [hypothesis_id, reading_id, user, text, created, updated, references]
+            comments.loc[len(comments)] = comment
 
     # comments['in_class'] = comments.user.map(lambda x: x in list(usernames))
     comments.index.name = "id"
---
+++ code/process_input_files.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    data_dir = Path("../data")
+    input_files = list(data_dir.glob("Hypothesis*.csv"))
+
+    # Parse input CSVs and create spreadsheets for processing
+    readings_cols = ["course", "course_title", "instructor", "email", "group", "group_name",
+                     "date", "month", "week", "title", "url"]
+    readings = pd.DataFrame(columns=readings_cols)
+
+    for f in input_files:
+        # reading info
+        df = pd.read_csv(f, parse_dates=["Date"])
+        df['month'] = df.Date.map(lambda x: x.month)
+        df['week'] = df.Date.map(lambda x: x.week)
+        df['week'] = df['week'] - min(df['week']) + 1
+
+        # course info
+        course = df["Course Number"][0]
+        course_title = df["Course Name"][0]
+        instructor = df["First Name"][0] + " " + df["Last Name "][0]
+        email = df["Email"][0]
+        groups = df['Hypothesis Groups'][0].split("\n")
+        group_ids = [group.split("/")[-2] for group in groups]
+        group_names = [group.split("/")[-1] for group in groups]
+
+        for ix, row in df.iterrows():
+            for group, group_name in zip(group_ids, group_names):
+                reading = [course, course_title, instructor, email, group, group_name,
+                           row['Date'], row['month'], row['week'], row['Title'], row['Link ']]
+                readings.loc[len(readings)] = reading
+
+    readings.index.name = "id"
+    readings.to_csv(data_dir / "readings.csv")
+
+    groups_cols = ["group", "group_name", "course", "course_title", "instructor", "email"]
+    groups = readings.groupby(groups_cols)['url'].count().reset_index().set_index("group")
+    groups.rename({'url': 'url_count'})
+    groups['comment_count'] = None
+    groups = groups.sort_values(by="course")
+    groups.to_csv(data_dir / "groups.csv")
